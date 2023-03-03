9ac5e1fe6f781781c0225946402f4981443e7972
Coder <sa3dideveloper21@gmail.com>
2023-03-03 07:27:00 -0800

    Java Loops

---
+++ src/BasicsOfJava/M0_JavaForLoops.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+
+public class M0_JavaForLoops {
+    public  static void main(String args[]){
+      //  Loops can execute a block of code as long as a specified condition is reached.
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+           
+        }
+    }
+}
