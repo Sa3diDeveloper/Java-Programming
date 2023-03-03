24e6a866e2edcb722ac6aa9b2324c1cdb39060b4
Coder <sa3dideveloper21@gmail.com>
2023-03-03 04:58:42 -0800

    Java num and flaot data type

--- src/BasicsOfJava/D_javaDataType.java
+++ src/BasicsOfJava/D_javaDataType.java
@@ -1,6 +1,6 @@
 package BasicsOfJava;
 
-public class javaDataType {
+public class D_javaDataType {
     public  static  void  main(String args[]){
         int myNum = 5;               // Integer (whole number)
         float myFloatNum = 5.99f;    // Floating point number
