adea0a51933a2be99c5231af8ea01976617e448f
Coder <sa3dideveloper21@gmail.com>
2023-03-03 07:31:11 -0800

    Java Break And Continues

--- src/BasicsOfJava/M0_JavaForLoops.java
+++ src/BasicsOfJava/M0_JavaForLoops.java
@@ -5,7 +5,7 @@ public class M0_JavaForLoops {
       //  Loops can execute a block of code as long as a specified condition is reached.
         for(int i=0;i<10;i++){
             System.out.println(i);
-           
+
         }
     }
 }
---
+++ src/BasicsOfJava/M3_JavaBreakAndContinues.java
@@ -0,0 +1,22 @@
+package BasicsOfJava;
+
+public class M3_JavaBreakAndContinues {
+    public  static  void  main(String args[]){
+        int i = 0;
+        while (i < 10) {
+            if (i == 4) {
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("******************************");
+        for (int j = 0; j < 10; j++) {
+            if (j == 4) {
+                continue;
+            }
+            System.out.println(j);
+        }
+    }
+}
