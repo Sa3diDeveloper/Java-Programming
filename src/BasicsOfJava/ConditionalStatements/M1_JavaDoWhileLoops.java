5a62d2324d476c51875a9e3fe1783cc10f51c1c3
Sa3di Programmer <sa3dopro1231239@gmail.com>
2023-03-11 15:14:08 +0300

    cinditional statements in java

---
+++ src/BasicsOfJava/ConditionalStatements/M1_JavaDoWhileLoops.java
@@ -0,0 +1,12 @@
+package BasicsOfJava.ConditionalStatements;
+
+public class M1_JavaDoWhileLoops {
+    public  static void main(String args[]){
+      //  Loops can execute a block of code as long as a specified condition is reached.
+        int i=1;
+       do{
+           System.out.println(i);
+           i++;
+       }while (i<10);
+    }
+}
