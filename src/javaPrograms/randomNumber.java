f45d2e12860ac35550c4af61af28106a66c3aa69
Sa3di Programmer <sa3dopro1231239@gmail.com>
2023-03-14 00:32:13 +0300

    genrate random number in java

---
+++ src/javaPrograms/randomNumber.java
@@ -0,0 +1,10 @@
+package javaPrograms;
+
+public class randomNumber {
+    public static void  main(String args[]){
+        System.out.println("1st Random Number: " + Math.random());
+        System.out.println("2nd Random Number: " + Math.random());
+        System.out.println("3rd Random Number: " + Math.random());
+        System.out.println("4th Random Number: " + Math.random());
+    }
+}
